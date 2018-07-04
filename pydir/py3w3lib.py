7a29b1a1206b269721c056d23028419347aeb8da
youtui <aiyf@linkedge.com.cn>
2018-07-04 18:34:41 +0800

    py3 w3lib学习

--- pydir/py3w3lib.py
+++ pydir/py3w3lib.py
@@ -1,6 +1,8 @@
 import os
 import w3lib.encoding
 import w3lib.html
+import w3lib.http
+import w3lib.url
 
 
 def test_str(dirname):
@@ -73,6 +75,42 @@ def test_str(dirname):
     # html实体转义，不是删除
     markup = w3lib.html.unquote_markup('<h1>&lt;')
     print(markup)
+
+    # 客户端应该使用带有“Bearer”HTTP授权方案的“Authorization”请求头部字段发起带有不记名令牌的身份验证请求。资源服务器必须支持此方法。
+    Authorization = w3lib.http.basic_auth_header('user','123456')
+    print(Authorization)
+
+    # 返回标头的原始HTTP标头表示
+    http_head = w3lib.http.headers_dict_to_raw({b'Content-type': b'text/html', b'Accept': b'gzip'})
+    print(http_head)
+
+    # 将原始标头（单个多行字节串）转换为字典
+    http_dict = w3lib.http.headers_raw_to_dict(http_head)
+    print(http_dict)
+
+    # 在给定网址中添加或修改参数
+    url = 'www.google.com'
+    new_url = w3lib.url.add_or_replace_parameter(url,'id','1')
+    new_url_2 = w3lib.url.add_or_replace_parameter(new_url, 'pid', '1')
+    print(new_url)
+    print(new_url_2)
+
+    # 如果给定路径名，则返回其文件URI，否则返回未修改
+    # uri 是一种资源
+    uri = w3lib.url.any_to_uri(file_content)
+    print(uri)
+
+    print('\n')
+
+    # UTF-8转换 + 非ASCII字符的百分比编码
+    ascii_url = w3lib.url.canonicalize_url(u'http://www.example.com/r\u00e9sum\u00e9')
+    print(ascii_url)
+
+    # 然后按值对查询参数进行排序
+    sort_url = w3lib.url.canonicalize_url('http://www.example.com/do?c=3&b=5&b=2&a=50')
+    print(sort_url)
+
+
 
 
 if __name__ == '__main__':
